8d9167b41650f74d63caacbd989636453632ffc6
Ashlyn <ashlyn@lilyx.io>
2022-03-04 20:56:40 -0700

    configured nuxt modules googlefonts and storyblok

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,6 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'storyblok-nuxt-tailwind-template',
@@ -18,7 +17,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['~/assets/css/global.css'],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -47,13 +46,25 @@ export default {
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios'
   ],
-
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+  // ----------------------------- //
+  // --- Module Configuration --- //
+  // ----------------------------- //
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/'
   },
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {}
+  googleFonts: {
+    families: {
+      'Francois+One': true,
+      Karla: [500, 700],
+      'Source+Code+Pro': true
+    }
+  },
+  storyblok: {
+    accessToken: 'process.env.STORYBLOK_API_KEY',
+    type: 'memory'
+  }
 }
