e3276b4b224c50579716d87d029c7e8f41f27d41
Ashlyn <ashlyn@lilyx.io>
2022-03-04 19:09:11 -0700

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,59 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'storyblok-nuxt-tailwind-template',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: ['~/assets/css/global.css'],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    '@nuxtjs/composition-api/module',
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+    // https://vueuse.org
+    '@vueuse/nuxt',
+    // https://github.com/harlan-zw/nuxt-webpack-optimisations
+    'nuxt-webpack-optimisations',
+    // https://image.nuxtjs.org
+    '@nuxt/image',
+    // https://github.com/nuxt-community/google-fonts-module
+    '@nuxtjs/google-fonts'
+  ],
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios'
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/'
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {}
+}
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'IndexPage',
+  setup() {
+    const test = 'Test Text'
+    return { test }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <p>{{ test }}</p>
+  </div>
+</template>
