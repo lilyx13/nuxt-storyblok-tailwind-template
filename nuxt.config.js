d0711d8d3e0c23d6f6908a3b65d564e3ed7ff162
Ashlyn <ashlyn@lilyx.io>
2022-03-06 04:27:46 -0700

    vue composition api works better than it's nuxt counterpart

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,14 +20,13 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['~/plugins/composition-api'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    '@nuxtjs/composition-api/module',
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
